1468779802a6ff0a1a4fc483f13d230696584d5b
torpakwastaken <yunusemrekoyunlu@gmail.com>
2023-08-11 07:34:06 +0300

    added text and other stuff.this thing taking shape

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const textElement = document.getElementById("text");
+const additionalText = document.querySelector(".additional-text"); // Select the additional text container
 const text =
   "Hello, this is a basic < typewriter > animation created by Y.Emre KOYUNLU.";
 let charIndex = 0;
@@ -7,8 +8,31 @@ function type() {
   if (charIndex < text.length) {
     textElement.textContent += text.charAt(charIndex);
     charIndex++;
-    setTimeout(type, 1);
+    setTimeout(type, 50); // Adjust the timeout as needed
+  } else {
+    // Animation is complete, reveal the additional text
+    additionalText.style.display = "block";
   }
 }
 
 type();
+
+const themeToggle = document.getElementById("themeToggle");
+const body = document.body;
+
+themeToggle.addEventListener("click", () => {
+  body.classList.toggle("dark-mode");
+
+  const isDarkMode = body.classList.contains("dark-mode");
+  if (isDarkMode) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+});
+
+// Check local storage for saved theme preference
+const savedTheme = localStorage.getItem("theme");
+if (savedTheme === "dark") {
+  body.classList.add("dark-mode");
+}
